55b1724cc08f80392ca3bd9d0985b674a53cbf6c
Richard Clark <richard@rvvup.com>
2023-12-18 15:20:41 +0000

    delete all the things

--- view/frontend/web/js/method/pay-by-card/card.js
+++
@@ -1,7 +0,0 @@
-require(['domReady!'],
-    function () {
-    let rvvupCardScript = window.rvvup_card_script;
-    require([rvvupCardScript], function(externalScript) {
-        window.SecureTrading = externalScript;
-    });
-});
