c7c104c9797b81f69561d3aa8e4f3adfe58eaaef
Michiel Gerritsen <michiel@controlaltdelete.nl>
2024-02-06 12:47:13 +0100

    Feature: Render remote payment icons

---
+++ src/Plugin/Hyva/Checkout/Model/MethodMetaData/IconRenderer/RenderExternalIcon.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rvvup\PaymentsHyvaCheckout\Plugin\Hyva\Checkout\Model\MethodMetaData\IconRenderer;
+
+use Hyva\Checkout\Model\MethodMetaData\IconRenderer;
+
+class RenderExternalIcon
+{
+    /**
+     * Hyvä does not support external images. So render it ourselves.
+     *
+     * @param IconRenderer $subject
+     * @param string $result
+     * @param string $url
+     * @return string
+     */
+    public function afterRenderAsImage(IconRenderer $subject, string $result, string $url): string
+    {
+        if (strpos($url, 'rvvup') === false || strpos($url, 'https://') === false) {
+            return $result;
+        }
+
+        return '<img class="h-11 w-11" src="' . $url . '" alt="Rvvup Payment Method" />';
+    }
+}
--- src/Plugin/Hyva/Checkout/Model/MethodMetaData/RenderMethodIcons.php
+++ src/Plugin/Hyva/Checkout/Model/MethodMetaData/RenderMethodIcons.php
@@ -5,9 +5,24 @@ declare(strict_types=1);
 namespace Rvvup\PaymentsHyvaCheckout\Plugin\Hyva\Checkout\Model\MethodMetaData;
 
 use Hyva\Checkout\Model\MethodMetaData;
+use Magento\Checkout\Model\Session;
+use Rvvup\Payments\Model\SdkProxy;
 
 class RenderMethodIcons
 {
+    private Session $checkoutSession;
+    private SdkProxy $sdkProxy;
+
+    private ?array $methods = null;
+
+    public function __construct(
+        Session $checkoutSession,
+        SdkProxy $sdkProxy
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->sdkProxy = $sdkProxy;
+    }
+
     public function afterCanRenderIcon(MethodMetaData $subject, bool $result): bool
     {
         $code = $subject->getMethod()->getCode();
@@ -21,7 +36,7 @@ class RenderMethodIcons
         }
 
         $subject->setData('icon', [
-            'svg' => $logo,
+            'src' => $logo,
         ]);
 
         return true;
@@ -29,9 +44,19 @@ class RenderMethodIcons
 
     private function getLogo(string $method): ?string
     {
-        // We get "rvvup_CARD" but we need "card"
-        $methodName = strtolower(substr($method, strlen('rvvup_')));
+        if ($this->methods === null) {
+            $quote = $this->checkoutSession->getQuote();
+            $this->methods = $this->sdkProxy->getMethods((string)$quote->getGrandTotal(), $quote->getQuoteCurrencyCode());
+        }
 
-        return 'rvvup_payments/' . $methodName;
+        // We get "rvvup_CARD" but we need "CARD"
+        $methodName = substr($method, strlen('rvvup_'));
+        foreach ($this->methods as $method) {
+            if ($method['name'] == $methodName) {
+                return $method['logoUrl'];
+            }
+        }
+
+        return null;
     }
 }
