ae186304bfde29b4ff6fd78fd812b0245af5f1ea
Aashwin Mohan <aashwin@rvvup.com>
2024-10-28 13:46:16 +0000

    Add tests

--- Test/End-2-End/pay_by_bank.spec.js
+++ Test/End-2-End/pay_by_bank.spec.js
@@ -12,7 +12,6 @@ test("Can place an order using pay by bank", async ({ page, browser }) => {
   await page.waitForTimeout(3000);
   await page.getByRole("button", { name: "Place order" }).click();
 
-  // Credit card form
   const frame = page.frameLocator("iframe.rvvup-modal");
   await frame.getByLabel("Mock Bank").click();
   await frame.getByRole("button", { name: "Continue on desktop" }).click();
@@ -30,4 +29,26 @@ test("Can place an order using pay by bank", async ({ page, browser }) => {
   expect(warningMessage).toContain(
     "Your payment is being processed and is pending confirmation. You will receive an email confirmation when the payment is confirmed.",
   );
+});
+
+
+test("Prevent close is handled on bank selection screen and can only be closed by pressing 'x'", async ({page}) => {
+  const visitCheckoutPayment = new VisitCheckoutPayment(page);
+  await visitCheckoutPayment.visit();
+
+  await page.getByLabel("Pay by Bank").click();
+  /** Add timeout to prevent clicking 'Place Order' too fast, which will result in
+   * failure to open popup modal
+   */
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", {name: "Place order"}).click();
+
+  const frame = page.frameLocator("iframe.rvvup-modal");
+  await expect(frame.getByLabel("Mock Bank")).toBeVisible();
+  await page.locator("#rvvup-modal div").first().click();
+  // Shouldn't close
+  await page.waitForTimeout(500);
+  await frame.getByTitle("Exit").click();
+
+  await expect(page.locator("#rvvup-modal div").first()).toBeHidden();
 });
