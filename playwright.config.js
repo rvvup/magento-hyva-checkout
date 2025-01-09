ae9d3df1ec262925d64693c70ac2fdf0523ea0b9
Aashwin Mohan <96258159+aashwin-rvvup@users.noreply.github.com>
2025-01-09 14:11:18 +0000

    Update playwright config to printSteps (#122)

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests. */
   workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list', {printSteps: true}], ['html', {open: 'never'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
